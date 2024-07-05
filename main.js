99dd7f645de81f0d1c1ad65d6d12aa3a71d94f1f
pakawat <tigerhad@hotmail.com>
2024-07-05 16:27:35 +0700

    .gitignore

--- main.js
+++ main.js
@@ -3,20 +3,40 @@ const path = require('node:path')
 const ipc = ipcMain
 
 const LdapClient = require('ldapjs-client');
+var client;
 
-// const { Client } = require('ldapts');
+Authen()
 
-// const client = new Client({
-//   url: 'ldaps://10.200.180.45',
-//   timeout: 0,
-//   connectTimeout: 0,
-//   tlsOptions: {
-//     minVersion: 'TLSv1.2',
-//   },
-//   strictDN: true,
-// });
-// var client = new LdapClient({ url: 'ldap://10.200.180.45' });
-// console.log('client', client);
+async function Authen(){
+  client = new LdapClient({ url: 'ldap://10.200.180.202' });
+  try {
+    await client.bind('Sathon\\Childoper' , 'P@ssw0rd')
+    console.log('client', client);
+    search()
+  } catch (e) {
+    console.log('Bind failed');
+  }
+}
+
+// search()
+
+async function search(){
+  var dn = 'dc=Sathon,dc=BKK,dc=LOCAL'
+  try {
+    const options = {
+      filter: '(&(objectClass=user)(cn=*))',
+      scope: 'sub',
+      attributes: ['uid', 'dn', 'cn','email']
+    };
+  
+    const entries = await client.search(dn, options);
+    console.log('entries', entries);
+
+  } catch (e) {
+    console.log(e);
+
+  }
+}
 
 const createWindow = () => {
   // Create the browser window.
@@ -31,7 +51,7 @@ const createWindow = () => {
       contextIsolation : false,
 
     }
-  })
+})
 
   mainWindow.loadFile('src/index.html')
 
@@ -42,23 +62,15 @@ const createWindow = () => {
 
 }
 
-// ddd();
-
-async function ddd() {
-
-  console.log('client', client);
-  var l = await client.bind('cn=root', 'p@ssw0rd');
-  console.log('l',l);
-
-}
-
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
     
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  
   })
+
 })
 
 app.on('window-all-closed', () => {
